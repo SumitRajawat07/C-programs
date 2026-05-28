8896a7beaea867db1ddcbef210bf198fac64af79
sumitrajawat <rajawatsumit579@gmail.com>
2026-05-28 23:59:55 +0700

    day - 8 string in c

---
+++ 8_STRINGS_C/2_programme.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char greetings[] = "hello world";
+    greetings[0] = 'j';
+
+    printf("%s",greetings);
+
+    return 0;
+}
---
+++ 8_STRINGS_C/3_programme.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    char carname[] = "volvo";
+    int i;
+    for ( i = 0; i < 5; ++i)
+    {
+        printf("%c\n",carname[i]);
+    }
+
+    return 0;
+    
+}
